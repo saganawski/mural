6205e4fd8ea78ae24970495ee3cf196e4d86d0a2
Ken <saganawski@gmail.com>
2020-11-02 14:18:21 -0600

    base project

---
+++ src/test/java/com/chicago/mural/MuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicago.mural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
